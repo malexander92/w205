629fc08dbafb7d50660cb8e112b7aeb365d5434f
Michael Alexander <Malexander@comscore.com>
2017-04-18 12:05:23 -0400

    final results

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -5,13 +5,16 @@ def finalresults(word):
 
 	conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 	cur = conn.cursor()
-    
-    cur.execute("SELECT count FROM tweetwordcount WHERE word=%s;", (word, ))
-    
-    uCount = cur.fetchall()[0]
-    uCount = uCount[0]
-    
-    conn.commit()
-    conn.close()
+
+	cur.execute("SELECT count FROM tweetwordcount WHERE word=%s;", (word, ))
+
+	if cur.rowcount == 0:
+		uCount = 0
+	else:
+		uCount = cur.fetchall()[0]
+		uCount = uCount[0]
+
+	conn.commit()
+	conn.close()
 
 	print "Total number of occurrences of " + word + ": " + str(uCount)
