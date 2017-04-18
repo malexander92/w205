46df334c10fe258a85e0a541b9d5e68e62807400
Michael Alexander <Malexander@comscore.com>
2017-04-18 01:12:19 -0400

    sql variables

--- exercise_2/tweetwordcount/src/bolts/wordcount.py
+++ exercise_2/tweetwordcount/src/bolts/wordcount.py
@@ -12,7 +12,7 @@ class WordCounter(Bolt):
 
     def process(self, tup):
         word = tup.values[0]
-        print word
+        print str(word)
         uWord = str(word)
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
