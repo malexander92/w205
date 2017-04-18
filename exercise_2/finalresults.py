64a77211a9e24bb4ccaa41e302e16f528c4d9e21
Michael Alexander <Malexander@comscore.com>
2017-04-18 12:19:55 -0400

    final results

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -1,23 +1,36 @@
 import psycopg2
 import sys
 
-def finalresults(word):
-	word = word.lower()
+def finalresults(word = None):
+	if word is None:
+		conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+		cur = conn.cursor()
+		cur.execute("SELECT word, count FROM tweetwordcount ORDER BY word DESC;")
 
-	conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-	cur = conn.cursor()
+		results = cur.fetchall()
 
-	cur.execute("SELECT count FROM tweetwordcount WHERE word=%s;", (word, ))
+		conn.commit()
+		conn.close()
 
-	if cur.rowcount == 0:
-		uCount = 0
+		for rec in results:
+			print rec
+
 	else:
-		uCount = cur.fetchall()[0]
-		uCount = uCount[0]
+		word = word.lower()
 
-	conn.commit()
-	conn.close()
+		conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+		cur = conn.cursor()
+		cur.execute("SELECT count FROM tweetwordcount WHERE word=%s;", (word, ))
 
-	print "Total number of occurrences of " + word + ": " + str(uCount)
+		if cur.rowcount == 0:
+			uCount = 0
+		else:
+			uCount = cur.fetchall()[0]
+			uCount = uCount[0]
+
+		conn.commit()
+		conn.close()
+
+		print "Total number of occurrences of \"" + word + "\": " + str(uCount)
 
 finalresults(sys.argv[1])
