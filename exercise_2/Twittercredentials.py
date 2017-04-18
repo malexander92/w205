49c25a89b7ecc1bad821623ae1295653d03e6949
Michael Alexander <Malexander@comscore.com>
2017-04-17 21:54:23 -0400

    twitter credentials added

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "jX1x7VBctOz5voSdPZprmgoSd";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "RBKp2OGD8NntAeN93ftVfpmv0G1KRII90fiPEdL79K5OCDq87Q";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "2827600059-SV4GzXzSMQmbiCrsLdigWbRjKPgsFDuRP9BY6Or";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "fQmZEYs7qNsyzUhQuYRIyNB55M76CePxFPW2xQyCqZvNr";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
