80ddc0b20f76c2e7096d836f0788c96ff7381b3a
Michael Alexander <Malexander@comscore.com>
2017-04-18 12:53:20 -0400

    histogram

--- exercise_2/histogram.py
+++ exercise_2/histogram.py
@@ -16,13 +16,24 @@ def histogram(k1, k2):
 	for rec in results:
 		print rec
 
+# Getting max count value
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
+cur.execute("SELECT MAX(count) FROM tweetwordcount;")
+
+maxCount = cur.fetchall()[0]
+maxCount = maxCount[0]
+
+conn.commit()
+conn.close()
+
 # Setting default values
 if len(sys.argv) == 1:
-	k1_sys = 5
-	k2_sys = 15
+	k1_sys = 1
+	k2_sys = maxCount
 elif len(sys.argv) == 2:
 	k1_sys = sys.argv[1]
-	k2_sys = 15
+	k2_sys = maxCount
 else:
 	k1_sys = sys.argv[1]
 	k2_sys = sys.argv[2]
