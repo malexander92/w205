3ffee95c438e51c91a5de04a1b7d325fa0aa9970
Michael Alexander <Malexander@comscore.com>
2017-04-18 16:06:24 -0400

    plot color

--- exercise_2/plot_top.py
+++ exercise_2/plot_top.py
@@ -19,7 +19,7 @@ def plot_top():
 	counts = [x[1] for x in results]
 	y_pos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 
-	plt.bar(y_pos, counts, align='center', color=blue, alpha=0.5)
+	plt.bar(y_pos, counts, align='center', color="blue", alpha=0.5)
 	plt.xticks(y_pos, words)
 	plt.ylabel('Count')
 	plt.xlabel('Top 20 Words')
